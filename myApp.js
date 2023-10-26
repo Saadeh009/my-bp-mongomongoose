e9e5a29054803025362d4bde582126bebbe863e5
Saadeh009 <saadehelias9@gmail.com>
2023-10-26 15:14:08 +0300

    capitalie p in Person

--- myApp.js
+++ myApp.js
@@ -5,10 +5,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const PersonSchema = new mongoose.Schema({
   name: {type: String, required: true},
   age: Number,
- favoriteFoods: [String]
+  favoriteFoods: [String]
 })
 
-let Person = mongoose.model('person', PersonSchema);
+let Person = mongoose.model('Person', PersonSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
