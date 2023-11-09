82fba547f5b8f5e1b361c7adbaf9b15de19587a2
Saadeh009 <saadehelias9@gmail.com>
2023-11-09 16:09:51 +0200

    find one and update

--- myApp.js
+++ myApp.js
@@ -59,13 +59,12 @@ const findEditThenSave = (personId, done) => {
   })
 };
 
-/* Testing for github operations */
-
-
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, res) => {
+    if(err) return console.log(err);
+    done(null, res);
+  })
 };
 
 const removeById = (personId, done) => {
